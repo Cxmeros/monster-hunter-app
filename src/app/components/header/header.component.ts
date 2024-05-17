15539bb7e295f07dc340dec90d14d47f5feebca8
Cxmeros <cxmeros118@gmail.com>
2024-05-16 22:40:52 -0600

    Retoques

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,11 +3,12 @@ import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SearchService } from '../../services/search.service';
 import { debounceTime } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [FormsModule, ReactiveFormsModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
@@ -15,11 +16,22 @@ export class HeaderComponent implements OnInit{
 
   showSearchInput: boolean = true;
 
+  router = inject(Router);
+
   searchControl = new FormControl();
 
   _searchService = inject(SearchService);
 
   ngOnInit(): void {
+
+    this.router.events.subscribe(event => {
+      if(this.router.url !== '/'){
+        this.showSearchInput = false;
+      } else {
+        this.showSearchInput = true;
+      }
+    });
+
     this.searchControl.valueChanges.pipe(debounceTime(500)).subscribe((value) => {
       console.log(value);
       this._searchService.searchMonster.next(value || '');
